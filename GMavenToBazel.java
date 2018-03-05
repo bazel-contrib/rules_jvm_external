17a9054214a38ec516e22f660bdbbfe591d9663e
yrom <yrom@qq.com>
2018-03-05 15:06:47 +0800

    Optimized codes, make generating file more faster.
    from 7 mins to 3 mins

--- GMavenToBazel.java
+++ GMavenToBazel.java
@@ -1,4 +1,12 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,12 +16,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  * A command line tool for generating Bazel workspace rules for all JARs and AARs on
@@ -21,7 +28,8 @@ import org.w3c.dom.NodeList;
  */
 public class GMavenToBazel {
 
-  private GMavenToBazel() {}
+  private GMavenToBazel() {
+  }
 
   private static final String MASTER_INDEX_URL =
       "https://dl.google.com/dl/android/maven2/master-index.xml";
@@ -41,55 +49,59 @@ public class GMavenToBazel {
 
   public static void main(String[] args) throws Exception {
     Document masterIndex = parseUrl(new URL(MASTER_INDEX_URL));
-    List<String> groupIds = getGroupdIds(masterIndex);
-    Map<String, String> repositoryNameToRuleType = new HashMap<>();
-    Map<String, String> repositoryNameToTargetName = new HashMap<>();
-    Map<String, String> repositoryNameToArtifactString = new HashMap<>();
-    Map<String, Set<String>> repositoryNameToRepositoryNameDeps = new HashMap<>();
-    for (String groupId : groupIds) {
-      Map<String, Set<String>> artifactsToVersions = getArtifactsToVersions(groupId);
-      for (String artifactId : artifactsToVersions.keySet()) {
-        for (String version : artifactsToVersions.get(artifactId)) {
-          Document pom = parseUrl(getPomUrl(groupId, artifactId, version));
-          Element project = (Element) pom.getElementsByTagName("project").item(0);
-          String packaging = getPackaging(project);
-          if (!PACKAGING_TO_RULE.containsKey(packaging)) {
+    Map<String, Artifact> artifacts = getGroupdIds(masterIndex)
+        .parallelStream()
+        .flatMap(groupId -> {
+          Map<String, Set<String>> artifactsToVersions = getArtifactsToVersions(groupId);
+          return artifactsToVersions.entrySet()
+              .stream()
+              .flatMap(entry -> {
+                String artifactId = entry.getKey();
+                Set<String> versions = entry.getValue();
+                return versions.stream().map(version -> new String[]{groupId, artifactId, version});
+              });
+        })
+        .map(ids -> {
+          String groupId = ids[0], artifactId = ids[1], version = ids[2];
+          try {
+            Document pom = parseUrl(getPomUrl(groupId, artifactId, version));
+
+            String packaging = getPackaging(pom);
             // We don't support APK packaging yet.
-            continue;
+            if (!PACKAGING_TO_RULE.containsKey(packaging)) {
+              return null;
+            }
+            Artifact artifact = new Artifact(groupId, artifactId, version, packaging);
+            artifact.dependencies = getDependencyRepositoryNames(pom);
+
+            return artifact;
+          } catch (Exception e) {
+            throw new RuntimeException(e);
           }
-          String repositoryName = getRepositoryName(groupId, artifactId, version);
-          repositoryNameToRuleType.put(repositoryName, PACKAGING_TO_RULE.get(packaging));
-          repositoryNameToTargetName.put(
-              repositoryName, getTargetName(groupId, artifactId, version, packaging));
-          repositoryNameToArtifactString.put(
-              repositoryName, getArtifactString(groupId, artifactId, version));
-          repositoryNameToRepositoryNameDeps.put(repositoryName, getDependencyRepositoryNames(pom));
-        }
-      }
-    }
+        }).filter(Objects::nonNull)
+        .collect(Collectors.toMap(artifact -> artifact.repositoryName, artifact -> artifact));
 
-
-    PrintWriter bzlWriter = new PrintWriter(new FileWriter(OUTPUT_FILE));
+    PrintWriter bzlWriter = new PrintWriter(new BufferedWriter(new FileWriter(OUTPUT_FILE), 32 * 1024));
     bzlWriter.println(
         "load('@bazel_tools//tools/build_defs/repo:maven_rules.bzl', 'maven_jar', 'maven_aar')");
     bzlWriter.println("def gmaven_rules():");
-    for (String repositoryName : repositoryNameToRuleType.keySet()) {
-      String ruleType = repositoryNameToRuleType.get(repositoryName);
-      String artifactString = repositoryNameToArtifactString.get(repositoryName);
+
+    for (String repositoryName : artifacts.keySet()) {
+      Artifact artifact = artifacts.get(repositoryName);
+      String artifactString = artifact.toArtifactString();
       bzlWriter.println();
-      bzlWriter.println(String.format("  %s(", ruleType));
+      bzlWriter.println(String.format("  %s(", artifact.rule()));
       bzlWriter.println(String.format("      name = '%s',", repositoryName));
       bzlWriter.println(String.format("      artifact = '%s',", artifactString));
       bzlWriter.println("      settings = '@gmaven_rules//:settings.xml',");
       bzlWriter.println("      deps = [");
-      for (String repositoryNameDep : repositoryNameToRepositoryNameDeps.get(repositoryName)) {
-        String targetNameDep = repositoryNameToTargetName.get(repositoryNameDep);
+      for (String repositoryNameDep : artifact.dependencies) {
+        Artifact targetNameDep = artifacts.get(repositoryNameDep);
         if (targetNameDep == null) {
           // our princess is in another castle!
-          bzlWriter.println(String.format("        # GMaven does not have %s" , repositoryNameDep));
+          bzlWriter.println(String.format("        # GMaven does not have %s", repositoryNameDep));
         } else {
-          bzlWriter.println(
-              String.format("        '%s',", repositoryNameToTargetName.get(repositoryNameDep)));
+          bzlWriter.println(String.format("        '%s',", targetNameDep.targetName()));
         }
       }
       bzlWriter.println("      ],");
@@ -100,7 +112,9 @@ public class GMavenToBazel {
   }
 
   private static Document parseUrl(URL url) throws Exception {
-    return documentBuilderFactory.newDocumentBuilder().parse(url.openStream());
+    try (InputStream in = new BufferedInputStream(url.openStream(), 8096)) {
+      return documentBuilderFactory.newDocumentBuilder().parse(in);
+    }
   }
 
   private static List<String> getGroupdIds(Document masterIndex) {
@@ -112,9 +126,13 @@ public class GMavenToBazel {
     return groupIds;
   }
 
-  private static Map<String, Set<String>> getArtifactsToVersions(String groupId)
-      throws Exception {
-    Document groupIndex = parseUrl(getGroupIndexUrl(groupId));
+  private static Map<String, Set<String>> getArtifactsToVersions(String groupId) {
+    Document groupIndex;
+    try {
+      groupIndex = parseUrl(getGroupIndexUrl(groupId));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
     Map<String, Set<String>> result = new HashMap<>();
     NodeList artifactNodes = groupIndex.getDocumentElement().getChildNodes();
     for (int index = 1; index < artifactNodes.getLength(); index += 2) {
@@ -128,22 +146,23 @@ public class GMavenToBazel {
   }
 
   private static URL getGroupIndexUrl(String groupId) throws MalformedURLException {
-    return new URL(String.format(GROUP_INDEX_TEMPLATE_URL, groupId.replaceAll("\\.", "/")));
+    return new URL(String.format(GROUP_INDEX_TEMPLATE_URL, groupId.replace('.', '/')));
   }
 
-  private static URL getPomUrl(String groupId, String artifactId, String version) throws Exception {
+  private static URL getPomUrl(String groupId, String artifactId, String version) throws MalformedURLException {
     return new URL(
         String.format(
             POM_TEMPLATE_URL,
-            groupId.replaceAll("\\.", "/"),
+            groupId.replace('.', '/'),
             artifactId,
             version,
             artifactId,
             version));
   }
 
-  private static String getPackaging(Element element) throws Exception {
-    NodeList packaging = element.getElementsByTagName("packaging");
+  private static String getPackaging(Document pom) throws Exception {
+    Element project = (Element) pom.getElementsByTagName("project").item(0);
+    NodeList packaging = project.getElementsByTagName("packaging");
     return packaging.getLength() > 0 ? packaging.item(0).getTextContent() : "jar";
   }
 
@@ -174,11 +193,6 @@ public class GMavenToBazel {
     return escape(String.format("%s_%s_%s", groupId, artifactId, version));
   }
 
-  private static String getTargetName(
-      String groupId, String artifactId, String version, String packaging) {
-    return String.format("@%s//%s", getRepositoryName(groupId, artifactId, version), packaging);
-  }
-
   private static String escape(String string) {
     return string.replaceAll("[.-]", "_");
   }
@@ -188,5 +202,33 @@ public class GMavenToBazel {
     String raw = element.getElementsByTagName("version").item(0).getTextContent();
     raw = raw.replaceAll("\\[|]", "");
     return raw.split(",")[0];
+  }
+
+  static class Artifact {
+    String groupId;
+    String artifactId;
+    String version;
+    String packaging;
+    Set<String> dependencies;
+    String repositoryName;
+    Artifact(String groupId, String artifactId, String version, String packaging) {
+      this.groupId = groupId;
+      this.artifactId = artifactId;
+      this.version = version;
+      this.packaging = packaging;
+      this.repositoryName = getRepositoryName(groupId, artifactId, version);
+    }
+
+    String toArtifactString() {
+      return getArtifactString(groupId, artifactId, version);
+    }
+
+    String targetName() {
+      return String.format("@%s//%s", repositoryName, packaging);
+    }
+
+    String rule() {
+      return PACKAGING_TO_RULE.get(packaging);
+    }
   }
 }
