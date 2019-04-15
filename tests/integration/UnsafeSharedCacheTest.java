faef4dffcb68ea7ea06be84346f0c8a02c70fe4a
Jin <jin@users.noreply.github.com>
2019-04-15 18:26:34 -0400

    Turn the shell integration tests into Java tests (#87)
    * Fix migration script and get it working on macOS (#89)
    
    Use upstream Robolectric integration with rules_jvm_external in the android_local_test example (#86)
    
    * Use upstream Robolectric integration with rules_jvm_external in the android_local_test example
    
    * Add robolectric's repo sha256
    
    Add a example for Spring Boot (#96)
    
    * Add a example for Spring Boot
    
    * Add note about tutorial code's license
    
    * Clean up
    
    * Add explicit top level dependencies to the WORKSPACE
    
    WIP java_test
    
    Add default paths for Coursier
    
    Update presubmits
    
    Undo workspace
    
    Disable unsafe shared cache test on Windows
    
    typo
    
    Fix tests on macOS
    
    Fix breakage on Linux
    
    Simplify.
    
    Fix tests
    
    Fix tests
    
    fix tests
    
    Fix tests
    
    try
    
    try again
    
    * Delete unused files
    
    * Reduce nesting
    
    * Use return instead of System.exit
    
    * Fix condition

---
+++ tests/integration/UnsafeSharedCacheTest.java
@@ -0,0 +1,52 @@
+package com.jvm.external;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class UnsafeSharedCacheTest {
+
+  private static String OS = System.getProperty("os.name").toLowerCase();
+
+  @Test
+  public void test_jarsOnClassPath_areInTheSharedCache() throws IOException, URISyntaxException {
+    // No support for symlinks on Windows. Let's skip that.
+      if (OS.indexOf("win") != -1) {
+          return;
+      }
+      ClassLoader cl = ClassLoader.getSystemClassLoader();
+      for (URL url : ((URLClassLoader) cl).getURLs()) {
+          URI uri = url.toURI();
+          String uriString = uri.toString();
+          if (uriString.contains("repo1.maven.org") || uriString.contains("jcentray.bintray.com")) {
+              if (OS.indexOf("mac") >= 0) {
+                  // Mac.
+                  // The jar's path is automatically resolved to the actual location on macOS.
+                  assertThat(uriString, containsString("Library/Caches/Coursier/v1"));
+              } else {
+                  // Linux.
+                  // Assert that the symlink points to the jar in the default Coursier shared cache location
+                  // https://github.com/coursier/coursier/blob/master/doc/docs/cache.md#location
+                  if (Files.isSymbolicLink(Paths.get(uri))) {
+                      // If it's a symlink, we'll need to resolve it twice: output base -> shared cache.
+                      Path symlinkDest = Files.readSymbolicLink(Files.readSymbolicLink(Paths.get(uri)));
+                      assertThat(symlinkDest.toString(), containsString(".cache/coursier/v1"));
+                  } else {
+                      assertThat(uriString, containsString(".cache/coursier/v1"));
+                  }
+              }
+          }
+      }
+  }
+
+}
