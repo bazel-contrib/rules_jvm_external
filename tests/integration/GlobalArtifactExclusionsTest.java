67c6f6df4666d5e36da6b8f7bf1d5d8b5a909517
Jingwen Chen <jin@crypt.sg>
2019-05-19 22:06:02 -0400

    rm unused classes

--- tests/integration/GlobalArtifactExclusionsTest.java
+++ tests/integration/GlobalArtifactExclusionsTest.java
@@ -1,12 +1,9 @@
 package com.jvm.external;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.ClassPath;
 import com.google.common.reflect.ClassPath.ClassInfo;
 import java.io.IOException;
 import org.junit.Test;
-import java.net.URL;
-import java.net.URLClassLoader;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.StringContains.containsString;
