8a5b1bfb58b37f8882ff569a9cec1100cc4e9aad
Jingwen Chen <jin@crypt.sg>
2019-05-19 22:04:37 -0400

    Make tests work with JDK 11

--- tests/integration/GlobalArtifactExclusionsTest.java
+++ tests/integration/GlobalArtifactExclusionsTest.java
@@ -1,6 +1,9 @@
 package com.jvm.external;
 
-
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
+import java.io.IOException;
 import org.junit.Test;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -11,11 +14,13 @@ import static org.hamcrest.core.StringContains.containsString;
 public class GlobalArtifactExclusionsTest {
 
   @Test
-  public void test_globallyExcludedArtifacts_notOnClassPath() {
-    ClassLoader cl = ClassLoader.getSystemClassLoader();
-    for (URL url : ((URLClassLoader) cl).getURLs()){
-      assertThat(url.getFile().toString(), not(containsString("org/codehaus/mojo/animal-sniffer-annotations")));
-      assertThat(url.getFile().toString(), not(containsString("com/google/j2objc/j2objc-annotations")));
+  public void test_globallyExcludedArtifacts_notOnClassPah() throws IOException {
+    ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    ClassPath classPath = ClassPath.from(classLoader);
+    ImmutableSet<ClassInfo> set = classPath.getTopLevelClasses();
+    for (ClassInfo ci : set) {
+      assertThat(ci.getName(), not(containsString("org.codehaus.mojo.animal_sniffer")));
+      assertThat(ci.getName(), not(containsString("com.google.j2objc.annotations")));
     }
   }
 
