97c8b821b9902dfd87b6d17c24331946b7aa024d
Justin Y Wei <jwei@lyft.com>
2020-01-14 18:59:15 -0800

    Re-add test that shouldn't have been removed

--- private/tools/java/rules/jvm/external/HasherTest.java
+++ private/tools/java/rules/jvm/external/HasherTest.java
@@ -14,6 +14,8 @@
 package rules.jvm.external;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.StringEndsWith.endsWith;
+import static org.hamcrest.collection.IsArrayWithSize.arrayWithSize;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -49,6 +51,23 @@ public class HasherTest {
     assertThat(
         Hasher.sha256(file),
         equalTo("7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069"));
+  }
+
+  @Test
+  public void sha256_multipleFiles() throws IOException, FileNotFoundException {
+    File file1 = writeFile("test-1.file", "Hello World!");
+    File file2 = writeFile("test-2.file", "Hello!");
+    File file3 = writeFile("test-3.file", "Hello World!");
+    File argsfile = writeFile("argsfile", "test-1.file\ntest-2.file\ntest-3.file");
+    String[] files = new String[]{file1.getPath(), file2.getPath(), file3.getPath()};
+
+    String result = Hasher.hashFiles(Stream.of(files));
+    String[] lines = result.split("\n");
+    assertThat(lines, arrayWithSize(3));
+
+    checkLine(lines[0], "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069", "test-1.file");
+    checkLine(lines[1], "334d016f755cd6dc58c53a86e183882f8ec14f52fb05345887c8a5edd42c87b7", "test-2.file");
+    checkLine(lines[2], "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069", "test-3.file");
   }
 
   @Test
@@ -105,5 +124,11 @@ public class HasherTest {
       out.print(contents);
     }
     return file;
+  }
+
+  private void checkLine(String line, String hash, String file) {
+    String[] parts = line.split("\\s+");
+    assertThat(parts[0], equalTo(hash));
+    assertThat(parts[1], endsWith(file));
   }
 }
