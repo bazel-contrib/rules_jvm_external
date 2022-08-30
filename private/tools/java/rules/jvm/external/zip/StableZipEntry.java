913c0372f89edfc1de1dbd600248a179beb30127
Simon Mavi Stewart <simon_stewart@apple.com>
2022-08-30 09:52:50 +0100

    Use stable zip entries in `MergeJars`
    This change also makes `StableZipEntry` public so that tooling
    outside of rje can use it. This is already done by Selenium:
    
    https://github.com/SeleniumHQ/selenium/blob/selenium-4.4.0/java/src/dev/selenium/tools/modules/ModuleGenerator.java#L324

--- private/tools/java/rules/jvm/external/zip/StableZipEntry.java
+++ private/tools/java/rules/jvm/external/zip/StableZipEntry.java
@@ -23,14 +23,26 @@ import java.util.zip.ZipEntry;
 
 public class StableZipEntry extends ZipEntry {
 
-  // Starts at 1980-01-01. We'll match what buck uses
+  // File time is taken from the epoch (1970-01-01T00:00:00Z), but zip files
+  // have a different epoch. Oof. Make something sensible up.
   private static final long DOS_EPOCH = Instant.parse("1985-02-01T00:00:00.00Z").toEpochMilli();
+  // ZIP timestamps have a resolution of 2 seconds.
+  // see http://www.info-zip.org/FAQ.html#limits
+  private static final long MINIMUM_TIMESTAMP_INCREMENT = 2000L;
+
 
   public StableZipEntry(String name) {
     super(name);
 
-    setTime(DOS_EPOCH);
-    setCreationTime(FileTime.fromMillis(DOS_EPOCH));
-    setLastModifiedTime(FileTime.fromMillis(DOS_EPOCH));
+    // Returns the normalized timestamp for a jar entry based on its name. This is necessary since
+    // javac will, when loading a class X, prefer a source file to a class file, if both files have
+    // the same timestamp. Therefore, we need to adjust the timestamp for class files to slightly
+    // after the normalized time.
+    // https://github.com/bazelbuild/bazel/blob/master/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/jarhelper/JarHelper.java#L124
+    long timestamp = name.endsWith(".class") ? DOS_EPOCH + MINIMUM_TIMESTAMP_INCREMENT : DOS_EPOCH;
+
+    setTime(timestamp);
+    setCreationTime(FileTime.fromMillis(timestamp));
+    setLastModifiedTime(FileTime.fromMillis(timestamp));
   }
 }
