166d2fb75c0158c7885f08ae901dcb568dea7017
Luka Cindro <3155827+lukaciko@users.noreply.github.com>
2022-09-16 16:18:06 +0200

    Fix timestamp hermeticity in StableZipEntry

--- private/tools/java/rules/jvm/external/zip/StableZipEntry.java
+++ private/tools/java/rules/jvm/external/zip/StableZipEntry.java
@@ -17,15 +17,15 @@
 
 package rules.jvm.external.zip;
 
-import java.nio.file.attribute.FileTime;
-import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.zip.ZipEntry;
 
 public class StableZipEntry extends ZipEntry {
 
   // File time is taken from the epoch (1970-01-01T00:00:00Z), but zip files
   // have a different epoch. Oof. Make something sensible up.
-  private static final long DOS_EPOCH = Instant.parse("1985-02-01T00:00:00.00Z").toEpochMilli();
+  private static final LocalDateTime DOS_EPOCH = LocalDateTime.of(1985,2,1,0,0);
   // ZIP timestamps have a resolution of 2 seconds.
   // see http://www.info-zip.org/FAQ.html#limits
   private static final long MINIMUM_TIMESTAMP_INCREMENT = 2000L;
@@ -34,15 +34,16 @@ public class StableZipEntry extends ZipEntry {
   public StableZipEntry(String name) {
     super(name);
 
+    long timestamp = DOS_EPOCH.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     // Returns the normalized timestamp for a jar entry based on its name. This is necessary since
     // javac will, when loading a class X, prefer a source file to a class file, if both files have
     // the same timestamp. Therefore, we need to adjust the timestamp for class files to slightly
     // after the normalized time.
     // https://github.com/bazelbuild/bazel/blob/master/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/jarhelper/JarHelper.java#L124
-    long timestamp = name.endsWith(".class") ? DOS_EPOCH + MINIMUM_TIMESTAMP_INCREMENT : DOS_EPOCH;
+    timestamp = name.endsWith(".class") ? timestamp + MINIMUM_TIMESTAMP_INCREMENT : timestamp;
 
+    // We only set the MS-DOS time and not the extended timestamp so the resulting Zip file doesn't
+    // encode the timezone. The method uses the system timezone internally for conversion.
     setTime(timestamp);
-    setCreationTime(FileTime.fromMillis(timestamp));
-    setLastModifiedTime(FileTime.fromMillis(timestamp));
   }
 }
