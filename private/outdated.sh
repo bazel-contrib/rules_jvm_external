44345c8ed17c5307ea5d5bc78cc2629ef28e266a
Simon Mavi Stewart <simon_stewart@apple.com>
2022-09-22 11:49:12 +0100

    Ensure `:outdated` target works in a modularised build

--- private/outdated.sh
+++ private/outdated.sh
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-if [ -f "private/tools/prebuilt/outdated_deploy.jar" ]; then
-    outdated_jar_path=private/tools/prebuilt/outdated_deploy.jar
-else
-    outdated_jar_path=../rules_jvm_external/private/tools/prebuilt/outdated_deploy.jar
-fi
+outdated_jar_path=$1
+artifacts=$2
+repositories=$3
 
-java {proxy_opts} -jar $outdated_jar_path ../{repository_name}/outdated.artifacts ../{repository_name}/outdated.repositories
+java {proxy_opts} -jar "$outdated_jar_path" "$artifacts" "$repositories"
