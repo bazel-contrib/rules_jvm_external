db31dba554f2e0ca274629cbc692384ba2b25060
Vincent Rose <vrose@confluent.io>
2024-05-01 17:04:07 -0700

    match indentation a bit better in MergeJars

--- private/tools/java/com/github/bazelbuild/rules_jvm_external/jar/MergeJars.java
+++ private/tools/java/com/github/bazelbuild/rules_jvm_external/jar/MergeJars.java
@@ -152,10 +152,10 @@ public class MergeJars {
                 allServices.computeIfAbsent(servicesName, key -> new TreeSet<>());
             String content = new String(ByteStreams.toByteArray(zis));
             List<String> commentsRemoved = Arrays
-                    .stream(content.split("\n"))
-                    .filter(l -> !l.trim().startsWith("#"))
-                    .filter(l -> !l.isEmpty())
-                    .collect(Collectors.toList());
+                .stream(content.split("\n"))
+                .filter(l -> !l.trim().startsWith("#"))
+                .filter(l -> !l.isEmpty())
+                .collect(Collectors.toList());
             services.addAll(commentsRemoved);
             continue;
           }
