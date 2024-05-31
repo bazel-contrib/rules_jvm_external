a611053ad2a0fe5a12a2d43c3f42acc33266c712
Chris Heisterkamp <cheister@squareup.com>
2024-05-31 11:37:04 -0700

    Update buildifier to 7.1.2

--- tests/integration/plugin_targets/ClassUsingTestOnlyAnnotationProcessor.java
+++ tests/integration/plugin_targets/ClassUsingTestOnlyAnnotationProcessor.java
@@ -10,13 +10,13 @@ import org.openjdk.jmh.infra.Blackhole;
 @Warmup(iterations = 2, time = 1)
 @Measurement(iterations = 5, time = 1)
 public class ClassUsingTestOnlyAnnotationProcessor {
-    @Benchmark
-    public void stringBuilder(final Blackhole blackhole) {
-        blackhole.consume(new StringBuilder().append("foo").append("bar").toString());
-    }
+  @Benchmark
+  public void stringBuilder(final Blackhole blackhole) {
+    blackhole.consume(new StringBuilder().append("foo").append("bar").toString());
+  }
 
-    @Benchmark
-    public void stringJoin(final Blackhole blackhole) {
-        blackhole.consume(String.join("", "foo", "bar"));
-    }
+  @Benchmark
+  public void stringJoin(final Blackhole blackhole) {
+    blackhole.consume(String.join("", "foo", "bar"));
+  }
 }
