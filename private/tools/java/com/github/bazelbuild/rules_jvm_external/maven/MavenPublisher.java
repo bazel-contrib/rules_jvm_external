108494ba05cde3dc74845ac8ee0d6aa47bb7d97d
jchen <jchen@snapchat.com>
2024-07-03 16:35:58 -0700

    code change

--- private/tools/java/com/github/bazelbuild/rules_jvm_external/maven/MavenPublisher.java
+++ private/tools/java/com/github/bazelbuild/rules_jvm_external/maven/MavenPublisher.java
@@ -21,6 +21,30 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.bcpg.BCPGOutputStream;
+import org.bouncycastle.openpgp.PGPCompressedData;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPObjectFactory;
+import org.bouncycastle.openpgp.PGPPrivateKey;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.PGPSecretKey;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.PGPSignatureGenerator;
+import org.bouncycastle.openpgp.PGPSignatureList;
+import org.bouncycastle.openpgp.PGPUtil;
+import org.bouncycastle.openpgp.jcajce.JcaPGPSecretKeyRing;
+import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+import org.bouncycastle.openpgp.operator.bc.BcPGPContentSignerBuilder;
+import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
+import org.bouncycastle.openpgp.operator.bc.BcPBESecretKeyDecryptorBuilder;
+import org.bouncycastle.openpgp.operator.bc.BcPGPContentVerifierBuilderProvider;
+import org.bouncycastle.openpgp.operator.bc.BcPGPDigestCalculatorProvider;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.UncheckedIOException;
+
 import com.github.bazelbuild.rules_jvm_external.ByteStreams;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -59,6 +83,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class MavenPublisher {
+  private static final int BUFFER = 4096;
 
   private static final Logger LOG = Logger.getLogger(MavenPublisher.class.getName());
   private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(1);
@@ -78,6 +103,10 @@ public class MavenPublisher {
     boolean gpgSign = Boolean.parseBoolean(System.getenv("GPG_SIGN"));
     Credentials credentials =
         new BasicAuthCredentials(System.getenv("MAVEN_USER"), System.getenv("MAVEN_PASSWORD"));
+    boolean useInMemoryPgpKeys = Boolean.parseBoolean(System.getenv("USE_IN_MEMORY_PGP_KEYS"));
+    String signingKey = System.getenv("PGP_SIGNING_KEY");
+    String signingPassword = System.getenv("PGP_SIGNING_PWD");
+    SigningMetadata signingMetadata = new SigningMetadata(gpgSign, useInMemoryPgpKeys, signingKey, signingPassword);
 
     List<String> parts = Arrays.asList(args[0].split(":"));
     if (parts.size() != 3) {
@@ -93,12 +122,12 @@ public class MavenPublisher {
 
     try {
       List<CompletableFuture<Void>> futures = new ArrayList<>();
-      futures.add(upload(repo, credentials, coords, ".pom", pom, gpgSign));
+      futures.add(upload(repo, credentials, coords, ".pom", pom, signingMetadata));
 
       if (mainArtifact != null) {
         String ext =
             com.google.common.io.Files.getFileExtension(mainArtifact.getFileName().toString());
-        futures.add(upload(repo, credentials, coords, "." + ext, mainArtifact, gpgSign));
+        futures.add(upload(repo, credentials, coords, "." + ext, mainArtifact, signingMetadata));
       }
 
       if (args.length > 3 && !args[3].isEmpty()) {
@@ -115,7 +144,7 @@ public class MavenPublisher {
                   coords,
                   String.format("-%s.%s", classifier, ext),
                   artifact,
-                  gpgSign));
+                  signingMetadata));
         }
       }
 
@@ -149,7 +178,7 @@ public class MavenPublisher {
       Coordinates coords,
       String append,
       Path item,
-      boolean gpgSign)
+      SigningMetadata signingMetadata)
       throws IOException, InterruptedException {
 
     String base =
@@ -174,10 +203,27 @@ public class MavenPublisher {
     uploads.add(upload(String.format("%s%s.md5", base, append), credentials, md5));
     uploads.add(upload(String.format("%s%s.sha1", base, append), credentials, sha1));
 
-    if (gpgSign) {
-      uploads.add(upload(String.format("%s%s.asc", base, append), credentials, sign(item)));
-      uploads.add(upload(String.format("%s%s.md5.asc", base, append), credentials, sign(md5)));
-      uploads.add(upload(String.format("%s%s.sha1.asc", base, append), credentials, sign(sha1)));
+    SigningMethod signingMethod = signingMetadata.getSigningMethod();
+    if (signingMethod.equals(SigningMethod.GPG)) {
+      uploads.add(upload(String.format("%s%s.asc", base, append), credentials, gpg_sign(item)));
+      uploads.add(upload(String.format("%s%s.md5.asc", base, append), credentials, gpg_sign(md5)));
+      uploads.add(upload(String.format("%s%s.sha1.asc", base, append), credentials, gpg_sign(sha1)));
+    } else if (signingMethod.equals(SigningMethod.PGP)) {
+      uploads.add(upload(
+              String.format("%s%s.asc", base, append),
+              credentials,
+              in_memory_pgp_sign(item, signingMetadata.signingKey, signingMetadata.signingPassword)
+      ));
+      uploads.add(upload(
+              String.format("%s%s.md5.asc", base, append),
+              credentials,
+              in_memory_pgp_sign(md5, signingMetadata.signingKey, signingMetadata.signingPassword)
+      ));
+      uploads.add(upload(
+              String.format("%s%s.sha1.asc", base, append),
+              credentials,
+              in_memory_pgp_sign(sha1, signingMetadata.signingKey, signingMetadata.signingPassword)
+      ));
     }
 
     return CompletableFuture.allOf(uploads.toArray(new CompletableFuture<?>[0]));
@@ -335,8 +381,8 @@ public class MavenPublisher {
     };
   }
 
-  private static Path sign(Path toSign) throws IOException, InterruptedException {
-    LOG.info("Signing " + toSign);
+  private static Path gpg_sign(Path toSign) throws IOException, InterruptedException {
+    LOG.info("Signing " + toSign + " with GPG");
 
     // Ideally, we'd use BouncyCastle for this, but for now brute force by assuming
     // the gpg binary is on the path
@@ -433,5 +479,172 @@ public class MavenPublisher {
 
     @Override
     public void refresh() {}
+  }
+
+  /**
+   * This method is based on the following Gradle code:
+   * <a href="https://cs.android.com/android-studio/gradle/+/master:platforms/software/security/src/main/java/org/gradle/security/internal/pgp/BaseInMemoryPgpSignatoryProvider.java">...</a>
+   * <a href="https://cs.android.com/android-studio/gradle/+/master:platforms/software/security/src/main/java/org/gradle/plugins/signing/signatory/pgp/PgpSignatory.java">...</a>
+   * <a href="https://cs.android.com/android-studio/gradle/+/master:platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/verification/writer/WriteDependencyVerificationFile.java;l=586?q=ArmoredOutputStream&ss=android-studio%2Fgradle">...</a>
+   * <a href="https://cs.android.com/android-studio/gradle/+/master:platforms/software/security/src/main/java/org/gradle/security/internal/SecuritySupport.java;l=65">...</a>
+   *
+   * @param toSign
+   * @param key
+   * @param password
+   * @return
+   * @throws IOException
+   */
+  private static Path in_memory_pgp_sign(Path toSign, String key, String password) throws IOException {
+    LOG.info("Signing " + toSign + " with in-memory PGP keys");
+
+    Path dir = Files.createTempDirectory("maven-sign");
+    Path signatureOutputPath = dir.resolve(toSign.getFileName() + ".asc");
+
+    PGPSecretKey pgpSecretKey;
+    // CI builder should have the key in the environment variable in Base64 format
+    try (InputStream in = PGPUtil.getDecoderStream(new ByteArrayInputStream(Base64.getDecoder().decode(key)))) {
+      pgpSecretKey = new JcaPGPSecretKeyRing(in).getSecretKey();
+    } catch (Exception e) {
+      throw new IOException("Could not read PGP secret key", e);
+    }
+
+    // Decrypt the PGPSecretKey to get a PGPPrivateKey
+    PGPPrivateKey privateKey = createPrivateKey(pgpSecretKey, password);
+
+    try {
+      // Create a PGPSignatureGenerator with the PGPPrivateKey
+      PGPSignatureGenerator signatureGenerator = new PGPSignatureGenerator(
+              new BcPGPContentSignerBuilder(pgpSecretKey.getPublicKey().getAlgorithm(), PGPUtil.SHA512)
+      );
+      signatureGenerator.init(PGPSignature.BINARY_DOCUMENT, privateKey);
+
+      // Read the input file and write its contents to the PGPSignatureGenerator
+      try (InputStream in = new BufferedInputStream(Files.newInputStream(toSign))) {
+        int ch;
+        while ((ch = in.read()) >= 0) {
+          signatureGenerator.update((byte) ch);
+        }
+      }
+
+      // Generate the signature
+      PGPSignature signature = signatureGenerator.generate();
+
+      // Write the signature to the output file
+      try (ArmoredOutputStream out =
+                   new ArmoredOutputStream(new BCPGOutputStream(Files.newOutputStream(signatureOutputPath)))
+      ) {
+        signature.encode(out, true /* forTransfer */);
+        out.flush();
+      }
+    } catch (PGPException e) {
+      throw new RuntimeException(e);
+    }
+
+    // Verify the signature
+    PGPSignatureList pgpSignatures = readSignatures(signatureOutputPath);
+    if (pgpSignatures == null) {
+      throw new IOException("Could not read PGP signatures");
+    }
+
+    for (PGPSignature signature : pgpSignatures) {
+      try {
+        if (!verify(toSign, signature, pgpSecretKey.getPublicKey())) {
+          throw new RuntimeException(
+                  String.format("Could not verify PGP signature for file %s!", toSign.getFileName())
+          );
+        } else {
+          LOG.info(String.format("PGP signature verified for file %s", toSign.getFileName()));
+        }
+      } catch (PGPException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    return signatureOutputPath;
+  }
+
+  public static String bytesToHex(byte[] bytes) {
+    return String.format("%0" + (bytes.length << 1) + "x", new BigInteger(1, bytes));
+  }
+
+  public static boolean verify(Path originFilePath, PGPSignature signature, PGPPublicKey publicKey) throws PGPException {
+    signature.init(new BcPGPContentVerifierBuilderProvider(), publicKey);
+
+    LOG.info(String.format("[Verify] Public Key Fingerprint: %s", bytesToHex(publicKey.getFingerprint())));
+
+    byte[] buffer = new byte[BUFFER];
+    int len;
+    try (InputStream in = new BufferedInputStream(Files.newInputStream(originFilePath))) {
+      while ((len = in.read(buffer)) >= 0) {
+        signature.update(buffer, 0, len);
+      }
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+    return signature.verify();
+  }
+
+  public static PGPSignatureList readSignatures(Path signaturePath) {
+    try (
+            InputStream stream = new BufferedInputStream(Files.newInputStream(signaturePath));
+            InputStream decoderStream = PGPUtil.getDecoderStream(stream)
+    ) {
+      return readSignatureList(decoderStream);
+    } catch (IOException | PGPException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static PGPSignatureList readSignatureList(InputStream decoderStream) throws IOException, PGPException {
+    PGPObjectFactory objectFactory = new PGPObjectFactory(decoderStream, new BcKeyFingerprintCalculator());
+    Object nextObject = objectFactory.nextObject();
+    if (nextObject instanceof PGPSignatureList) {
+      return (PGPSignatureList) nextObject;
+    } else if (nextObject instanceof PGPCompressedData) {
+      return readSignatureList(((PGPCompressedData) nextObject).getDataStream());
+    } else {
+      LOG.warning(String.format("Skip parsing signature list in %s.", nextObject == null ? "invalid file" : nextObject.getClass()));
+      return null;
+    }
+  }
+
+  private static PGPPrivateKey createPrivateKey(PGPSecretKey secretKey, String password) {
+    try {
+      PBESecretKeyDecryptor decryptor = new BcPBESecretKeyDecryptorBuilder(new BcPGPDigestCalculatorProvider()).build(password.toCharArray());
+      return secretKey.extractPrivateKey(decryptor);
+    } catch (PGPException e) {
+      throw new RuntimeException("Could not extract private key", e);
+    }
+  }
+
+  private enum SigningMethod {
+    GPG,
+    PGP,
+    NONE
+  }
+
+  private static class SigningMetadata {
+    private final String signingKey;
+    private final String signingPassword;
+    private final SigningMethod signingMethod;
+
+    public SigningMetadata(Boolean gpgSign, Boolean useInMemoryPgpKeys, String signingKey, String signingPassword) {
+      this.signingKey = signingKey;
+      this.signingPassword = signingPassword;
+      if (gpgSign && useInMemoryPgpKeys) {
+        throw new IllegalArgumentException("Cannot use in-memory PGP keys with GPG signing");
+      }
+      if (gpgSign) {
+        this.signingMethod = SigningMethod.GPG;
+      } else if (useInMemoryPgpKeys) {
+        this.signingMethod = SigningMethod.PGP;
+      } else {
+        this.signingMethod = SigningMethod.NONE;
+      }
+    }
+
+    public SigningMethod getSigningMethod() {
+      return signingMethod;
+    }
   }
 }
