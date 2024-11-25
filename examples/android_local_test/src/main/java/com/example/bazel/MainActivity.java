874cc79c3a1fd51678112467233bfb94ade488bc
utzcoz <43091780+utzcoz@users.noreply.github.com>
2024-11-25 12:04:54 +0800

    Bump Robolectric to 4.14.1 (#1286)
    1. Bump Robolectric to 4.14.1 including robolectric-bazel.
    2. Update pinned maven_install.json.
    3. Bump SDK to 29 for related examples.
    4. Remove some unused dependencies like androidx.annotation.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- examples/android_local_test/src/main/java/com/example/bazel/MainActivity.java
+++ examples/android_local_test/src/main/java/com/example/bazel/MainActivity.java
@@ -15,12 +15,11 @@
 package com.example.bazel;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
   @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
