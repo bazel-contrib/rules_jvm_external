aa34b3f323f830e7b7a2132aa07d7b1de3500d1b
Simon Mavi Stewart <simon.m.stewart@gmail.com>
2026-06-03 23:22:28 +0100

    Bump the deps in the Spring Boot examples (#1580)

--- examples/spring_boot/src/test/java/hello/HelloControllerIT.java
+++ examples/spring_boot/src/test/java/hello/HelloControllerIT.java
@@ -9,13 +9,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 public class HelloControllerIT {
 
   @LocalServerPort private int port;
